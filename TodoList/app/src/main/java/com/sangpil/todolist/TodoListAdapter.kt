f1583e8fdd05991b2267c01941bc9ef02ad900e2
prgman <52586558+prgman@users.noreply.github.com>
2019-08-10 08:51:22 +0900

    Add files via upload

---
+++ TodoList/app/src/main/java/com/sangpil/todolist/TodoListAdapter.kt
@@ -0,0 +1,46 @@
+package com.sangpil.todolist
+
+import android.text.format.DateFormat
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import io.realm.OrderedRealmCollection
+import io.realm.RealmBaseAdapter
+
+class TodoListAdapter(realmResult: OrderedRealmCollection<Todo>): RealmBaseAdapter<Todo>(realmResult){
+
+    class TodoViewHolder(view: View){
+        val dateTextView : TextView = view.findViewById(R.id.text1)
+        val textTextView : TextView = view.findViewById(R.id.text2)
+    }
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val vh: TodoViewHolder
+        val view: View
+
+        if(convertView == null){
+            view = LayoutInflater.from(parent?.context).inflate(R.layout.item_todo, parent, false)
+
+            vh = TodoViewHolder(view)
+            view.tag = vh
+        }else {
+            view = convertView
+            vh = view.tag as TodoViewHolder
+        }
+
+        if(adapterData != null){
+            val item = adapterData!![position]
+            vh.textTextView.text = item.title
+            vh.dateTextView.text = DateFormat.format("yyyy/MM/dd", item.date)
+        }
+        return view
+    }
+
+    override fun getItemId(position: Int): Long {
+        if(adapterData != null){
+            return adapterData!![position].id
+        }
+        return super.getItemId(position)
+    }
+
+}
